82e42bd7c832f370b3cb4bea6c2aa27c1f6ea082
LeotheLeo02 <86019055+LeotheLeo02@users.noreply.github.com>
2022-09-22 20:30:23 -0500

    Changes

--- Scholar+/ContentView.swift
+++ Scholar+/ContentView.swift
@@ -19,40 +19,55 @@ struct ContentView: View {
     @StateObject private var viewmodel = LocationViewModel()
     @State private var add = false
     @State private var name = ""
+    @FocusState var focus
     var body: some View {
         ZStack{
             Map(coordinateRegion: $viewmodel.region, showsUserLocation: true, annotationItems: annotations) { ann in
-            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: ann.latitude, longitude: ann.longitude)) {
-                Text(ann.name!)
-            }
+                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: ann.latitude, longitude: ann.longitude)) {
+                    Text(ann.name!)
+                }
             }.onAppear(){
                 viewmodel.CheckLocation()
             }
+            VStack{
             if add{
-                TextField("Name of Location", text: $name, axis: .vertical)
-                    .onSubmit {
-                        let newannotation = Annotation(context: viewContext)
-                        newannotation.latitude = viewmodel.region.center.latitude
-                        newannotation.longitude = viewmodel.region.center.longitude
-                        newannotation.name = name
-                        do {
-                            try viewContext.save()
-                        } catch {
-                            // Replace this implementation with code to handle the error appropriately.
-                            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                            let nsError = error as NSError
-                            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                HStack{
+                    TextField("Name of Location", text: $name, axis: .vertical)
+                        .focused($focus)
+                        .onSubmit {
+                            let newannotation = Annotation(context: viewContext)
+                            newannotation.latitude = viewmodel.region.center.latitude
+                            newannotation.longitude = viewmodel.region.center.longitude
+                            newannotation.name = name
+                            do {
+                                try viewContext.save()
+                            } catch {
+                                // Replace this implementation with code to handle the error appropriately.
+                                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                                let nsError = error as NSError
+                                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                            }
+                            name = ""
+                            add.toggle()
                         }
-                        name = ""
-                    }
+                }.padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(20)
+                    .padding()
             }
-            Circle()
-                .fill(.blue)
-                .opacity(0.3)
-                .frame(width: 32,height: 32)
-                .onTapGesture {
-                    add.toggle()
+                if !add{
+                    Circle()
+                        .fill(.blue)
+                        .opacity(0.3)
+                        .frame(width: 32,height: 32)
+                        .onTapGesture {
+                            withAnimation{
+                                add.toggle()
+                                focus.toggle()
+                            }
+                        }
                 }
+        }
     }
     }
 
@@ -88,12 +103,6 @@ struct ContentView: View {
 //    }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
